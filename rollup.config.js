c7891dba12cafe9d3a7a6c275be234f703011941
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-05-12 21:27:45 +0200

    init 3D scene

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,22 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourcemaps from 'rollup-plugin-sourcemaps'
 import copy from 'rollup-plugin-copy'
+import css from 'rollup-plugin-import-css'
 
 export default {
   input: 'src/index.js',
-  output: [
-    { file: 'dist/bundle.js', format: 'iife' },
-  ],
+  output: [{ file: 'dist/bundle.js', format: 'iife' }],
   external: [],
   watch: {
     include: 'src/**',
   },
   plugins: [
+    css(),
     commonjs(),
     nodeResolve(),
     sourcemaps(),
     copy({
-      targets: [
-          { src: `index.html`, dest: `dist` },
-      ]
-    })
+      targets: [{ src: `index.html`, dest: `dist` }],
+    }),
   ],
 }
