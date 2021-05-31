03255e4cdf9bb80dbd16b317defe69fa10fd15bf
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-05-31 13:02:06 +0200

    feat: add gzip compression

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const path = require('path')
+const compression = require('compression')
 const express = require('express')
 
 const app = express()
 
 const PORT = process.env.PORT || 8080
 
+app.use(compression())
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get(['*', '/works/*'], (req, res) => {
